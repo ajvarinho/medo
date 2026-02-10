1cb9a7efd9a7e432c21bfe8b3edcd4fd10a28e3e
nidjasim <polamekkartnija@gmail.com>
2026-02-10 20:16:36 +0100

    qwe

--- index.js
+++ index.js
@@ -27,20 +27,20 @@ const duration = 1500; // your animation time in ms
 let count = 3000;
 let count_zoom = 1500;
 
-medeki.forEach((element, index)=>{
+// medeki.forEach((element, index)=>{
 
-    // setTimeout(() => {
-    //     console.log("Delayed for 1 second.", element, index);
-    //     element.classList.add('start');
-    // }, count);
-    // count += 1500;
-    // console.log(count);
+//     // setTimeout(() => {
+//     //     console.log("Delayed for 1 second.", element, index);
+//     //     element.classList.add('start');
+//     // }, count);
+//     // count += 1500;
+//     // console.log(count);
 
-    setTimeout(() => {
-        //element.classList.remove('start')
-        element.classList.add('animate');
-        console.log(element, 'done')
-    }, count);
-    count += 2000;
-    console.log(count)
-});
+//     setTimeout(() => {
+//         //element.classList.remove('start')
+//         element.classList.add('animate');
+//         console.log(element, 'done')
+//     }, count);
+//     count += 2000;
+//     console.log(count)
+// });
